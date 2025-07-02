3bb92c7df386f949317b483cb982ffb0d3c09e23
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-07-02 10:07:26 -0300

    :tada: init:  Inicialização do projeto

---
+++ src/main/java/com/jefferson/lima/domesticTask/DomesticTaskApplication.java
@@ -0,0 +1,13 @@
+package com.jefferson.lima.domesticTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomesticTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomesticTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefferson/lima/domesticTask/DomesticTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefferson.lima.domesticTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesticTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
