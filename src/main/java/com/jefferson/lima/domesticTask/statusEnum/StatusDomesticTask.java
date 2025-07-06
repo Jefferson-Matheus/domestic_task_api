0dbc91c8f178f79a4b00b0a74a6dc26d4d92a577
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-07-06 12:21:54 -0300

    :sparkles: feat: Criação do enum de status

--- src/main/java/com/jefferson/lima/domesticTask/statusEnum/StatusDomesticTask.java
+++ src/main/java/com/jefferson/lima/domesticTask/statusEnum/StatusDomesticTask.java
@@ -1,4 +1,23 @@
 package com.jefferson.lima.domesticTask.statusEnum;
 
 public enum StatusDomesticTask {
+    TREND(1, "Em andamento"),
+    FINISHED(2, "Finalizada"),
+    PENDENT(3, "Pendente");
+
+    private int code;
+    private String description;
+
+    StatusDomesticTask(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
