5d0b563ffe3f6045aab821ad2a98b7fe28247c02
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-11-14 11:28:32 -0300

    :sparkles: feat: Criação dos metodos de serviços para atualização de nome e estado da tarafa

--- src/main/java/com/jefferson/lima/domesticTask/services/Domesticktaskservice.java
+++ src/main/java/com/jefferson/lima/domesticTask/services/Domesticktaskservice.java
@@ -4,6 +4,7 @@ package com.jefferson.lima.domesticTask.services;
 import com.jefferson.lima.domesticTask.dtos.Domestictaskdto;
 import com.jefferson.lima.domesticTask.entities.DomesticTask;
 import com.jefferson.lima.domesticTask.repositories.Taskrepository;
+import com.jefferson.lima.domesticTask.statusEnum.StatusDomesticTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,5 +23,22 @@ public class Domesticktaskservice {
         List<Domestictaskdto> domesticTasksInformations = domesticTasksResult.stream().map(x -> new Domestictaskdto(x)).toList();
 
         return domesticTasksInformations;
+    }
+
+    public void updateToFinishedSatus(long idTask){
+        DomesticTask taskSelected = taskrepository.findById(idTask).orElseThrow(() -> new RuntimeException("T"));
+
+        taskSelected.setStatus(StatusDomesticTask.FINISHED);
+
+        taskrepository.save(taskSelected);
+    }
+    
+    
+    public void upadateDomesticTaskName(long idTask, String taskName) {
+    	DomesticTask taskSelected = taskrepository.findById(idTask).orElseThrow(() -> new RuntimeException("T"));
+    	
+    	taskSelected.setName(taskName);
+    	
+    	taskrepository.save(taskSelected);
     }
 }
